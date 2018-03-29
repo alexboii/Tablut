497b8dcff43b83349d52ba7a9b7e741946bcddf0
Alex B <potteref@gmail.com>
2018-03-28 20:42:06 -0400

    first commit

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -1,11 +1,21 @@
 package student_player;
 
 import boardgame.Move;
+import coordinates.Coord;
+import coordinates.Coordinates;
 import tablut.TablutBoardState;
+import tablut.TablutMove;
 import tablut.TablutPlayer;
 
-/** A player file submitted by a student. */
+import java.util.List;
+import java.util.Random;
+
+/**
+ * A player file submitted by a student.
+ */
 public class StudentPlayer extends TablutPlayer {
+
+    private Random rand = new Random(1848);
 
     /**
      * You must modify this constructor to return your student number. This is
@@ -13,7 +23,7 @@ public class StudentPlayer extends TablutPlayer {
      * associate you with your agent. The constructor should do nothing else.
      */
     public StudentPlayer() {
-        super("xxxxxxxxx");
+        super("260684228");
     }
 
     /**
@@ -27,10 +37,104 @@ public class StudentPlayer extends TablutPlayer {
         // strategies...
         MyTools.getSomething();
 
-        // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
+        List<TablutMove> options = boardState.getAllLegalMoves();
 
+        // Is random the best you can do?
+        TablutMove myMove = options.get(rand.nextInt(options.size()));
+
+        TablutBoardState randomState = (TablutBoardState) boardState.clone();
+        randomState.processMove(myMove);
+        int opponent = randomState.getOpponent();
+        int min = randomState.getNumberPlayerPieces(opponent);
+        int startingState = min;
+
+        // we set current state as root
+        for (int i = 0; i < options.size(); i++) {
+            TablutBoardState cloneBS = (TablutBoardState) boardState.clone();
+
+            TablutMove currentMove = options.get(i);
+
+            cloneBS.processMove(currentMove);
+
+            int result = this.alphaBeta(cloneBS, 3, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+
+            if (result <= min) {
+                myMove = currentMove;
+                min = result;
+            }
+        }
+
+        System.out.println("RESULT = " + min);
+        System.out.println(player_id);
+        
+        if (player_id == TablutBoardState.SWEDE && startingState == min) {
+            Coord kingPos = boardState.getKingPosition();
+
+            // Don't do a move if it wouldn't get us closer than our current position.
+            int minDistance = Coordinates.distanceToClosestCorner(kingPos);
+
+            // Iterate over moves from a specific position, the king's position!
+            for (TablutMove move : boardState.getLegalMovesForPosition(kingPos)) {
+                /*
+                 * Here it is not necessary to actually process the move on a copied boardState.
+                 * Note that it is more efficient NOT to copy the boardState. Consider this
+                 * during implementation...
+                 */
+                int moveDistance = Coordinates.distanceToClosestCorner(move.getEndPosition());
+                if (moveDistance < minDistance) {
+                    minDistance = moveDistance;
+                    myMove = move;
+                }
+            }
+        }
+        
+        
         // Return your move to be processed by the server.
         return myMove;
     }
+
+    private int alphaBeta(TablutBoardState board, int depth, int alpha, int beta, boolean maximizingPlayer) {
+        // TODO: add an "isWinner" clause or "isTerminal"
+        if (depth == 0) {
+            int opponent = board.getOpponent();
+            int result = board.getNumberPlayerPieces(opponent);
+            return result;
+        }
+
+        List<TablutMove> options = board.getAllLegalMoves();
+
+
+        if (maximizingPlayer) {
+            for (TablutMove move : options) {
+                TablutBoardState cloneBS = (TablutBoardState) board.clone();
+                cloneBS.processMove(move);
+
+                alpha = Math.max(alpha, alphaBeta(cloneBS, depth - 1, alpha, beta, false));
+
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+
+            return alpha;
+        } else {
+            for (TablutMove move : options) {
+                TablutBoardState cloneBS = (TablutBoardState) board.clone();
+                cloneBS.processMove(move);
+
+                beta = Math.max(alpha, alphaBeta(cloneBS, depth - 1, alpha, beta, true));
+
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+
+            return beta;
+        }
+    }
+
+    private boolean checkTerminal(TablutBoardState board) {
+        return board.getWinner() == player_id || board.getWinner() == board.getOpponent();
+    }
+
 }
